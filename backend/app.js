ac4e6e81ff94d87511705d70763381059febf2da
abhishekrgadad <abhishekgadad485@gmail.com>
2024-12-30 21:30:21 +0530

    dependenices installed and first file created

--- backend/app.js
+++ backend/app.js
@@ -4,11 +4,12 @@ const app = express();
 
 const PORT = 3000;
 
+//get the data
 app.get('/',(req,res)=>{
     res.send('<h1>Welcome to Students API</h1>')
 })
 
-
+//listen to make the port into listening means working
 app.listen(PORT, (error)=>{
     if(!error){
         console.log("Server is successfully running at port", PORT);
