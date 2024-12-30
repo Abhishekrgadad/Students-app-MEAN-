153f0296c0f1a8a673b733802af98bdca44f92c0
abhishekrgadad <abhishekgadad485@gmail.com>
2024-12-30 22:13:45 +0530

    Added cors, parser and route api  to app.js

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,20 @@
 const express = require('express');
-
+const cors = require('cors');
 const app = express();
 
 const PORT = 3000;
+
+//enable cors
+app.use(cors());
+
+//enable json parser
+app.use(express.json());
+
+//routes to student API
+const studentRoutes = require('./routes/students');
+
+//use the routes
+app.use('/api/students', studentRoutes);
 
 //get the data
 app.get('/',(req,res)=>{
